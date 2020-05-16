b337d8eaa78e778237e566c38eeb5f21cfd84623
dlamaral <55581404+dlamaral@users.noreply.github.com>
2020-05-16 12:47:00 +0100

    Add files via upload

---
+++ cap.py
@@ -0,0 +1,11 @@
+'''
+Script that capitalizes the 1st letter of a string
+'''
+
+def cap_text(text):
+    '''
+    Input a string
+    Output the capitalized string
+    '''
+    return text.title()
+    
